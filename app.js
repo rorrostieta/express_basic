12db2d8c34344077aa154a5b219bfbb1b35dcc65
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-23 14:50:43 -0500

    update: express file to initate

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const express = require('express')
 const app = express()
 // Puerto en el que vamos a ver nuestra app: localhost:3000
 const port = 3000
+
+// path inicial, respondera a la url localhost:3000
+app.get('/', (req, res)=>{
+  res.send('Hello World')
+})
+
+// Con esto inicializamos la const app = express()
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
