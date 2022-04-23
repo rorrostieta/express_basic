28d93d3ab4263e0cc42036774787ac58603e2ed8
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-23 14:47:20 -0500

    add: app.js initiating express app

---
+++ app.js
@@ -0,0 +1,6 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en el que vamos a ver nuestra app: localhost:3000
+const port = 3000
